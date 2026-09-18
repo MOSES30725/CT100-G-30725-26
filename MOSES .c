564cb26cb9c2bbea5ef54b59b49087498087d02f
MOSES30725 <barakaraymond84@gmail.com>
2026-09-18 10:54:47 +0300

    Add files via upload

---
+++ MOSES .c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+int main()
+{
+    printf("mathematics for science \n");
+    printf("essentials of accounting\n");
+    printf("computer application\n");
+    printf("programming and algorithm\n"); 
+    return 0;
+}
