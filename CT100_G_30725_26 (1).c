eff85be852b6a578e8226899ae44721fa48e45ed
MOSES30725 <barakaraymond84@gmail.com>
2026-09-18 13:30:48 +0300

    Add files via upload

---
+++ CT100_G_30725_26 (1).c
@@ -0,0 +1,19 @@
+//program to display units of bachelor of science in information technology 
+/*
+Name:Moses
+Reg No :CT100/G/30725/26
+Description:unit offered
+Date : 17/9/2026
+version 1
+*/
+#include <stdio.h>
+int main ()
+{
+ printf("mathematics for science\n");
+ printf("Programming and algorithm\n");
+ printf("essentials of accounting\n");
+ printf("Analogue electronics\n");
+ printf("computer application\n");
+ 
+ return 0 ;
+ }
